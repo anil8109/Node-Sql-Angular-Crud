3cfe150fc4302904916c756fa1b4c6729e6070f4
anil8109 <anil810966@gmail.com>
2024-01-13 04:53:35 +0530

    Added constants message and values files.

---
+++ backend/constant/errorMessages.js
@@ -0,0 +1,14 @@
+module.exports.errorMessages = {
+  invalidRole: {
+    title: 'Invalid Role',
+    message: 'Role accepts only admin and user'
+  },
+  unExpectedError: {
+    title: 'Unexpected Error',
+    message: 'The operation could not perform'
+  },
+  missingParameters: {
+    title: 'User details are missing',
+    message: 'name, email, role and address are required'
+  }
+};
---
+++ backend/constant/messages.js
@@ -0,0 +1,14 @@
+module.exports.messages = {
+  usersFetched: 'Users Fetched Successfully',
+  unableToFetchUsers: 'Unable to select Users',
+  usesNotFound: 'Users not found',
+  couldNotAddUser: 'Could not add user',
+  duplicateUserFound: 'User Already Found with the email you entered',
+  userAdded: 'User Added Successfully',
+  couldNotEdit: 'Could not edit user',
+  userNotFound: 'User Not Found',
+  userUpdated: 'User data is successfully updated',
+  userDataIsDeleted: 'Users data is deleted',
+  unableToDeleteUser: 'Could not delete the user',
+  userDataFetched: 'Users data is fetched'
+};
